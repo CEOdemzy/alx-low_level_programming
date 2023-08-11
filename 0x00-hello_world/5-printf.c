ca7212459b462f9d3af4eabc4fcfef36b6ccbce7
CEOdemzy <ud.demzy@gmail.com>
2023-08-10 23:18:27 -0700

    5-prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Prints out a string to stdout.
+ *
+ *  Return: Always 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0); /* Return 0 to indicate successful execution */
+}
