23b740d183e17ed90679a79a624acfd47f9bff05
CEOdemzy <ud.demzy@gmail.com>
2023-08-11 10:57:18 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,6 @@ int main(void)
 		printf("Last digit of %d and is %d is 0\n", n, m);
 	else if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
-	printf("\n");
-
+	
 	return (0);
 }
