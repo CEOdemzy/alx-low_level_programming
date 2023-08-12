2a9046af8723521adcc55570df9dda162d323670
CEOdemzy <ud.demzy@gmail.com>
2023-08-12 01:54:53 -0700

    two numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 
-int main() {
-    int i = '0';
+/**
+ * main - main function
+ *
+ * Return: always 0 success
+ *
+ */
 
-    while (i <= '7') {
-        int j = i + 1;
+int main(void)
+{
+	int i = '0';
 
-        while (j <= '8') {
-            int k = j + 1;
-
-            while (k <= '9') {
-                putchar(i);
-                putchar(j);
-                putchar(k);
-
-                if (!(i == '7' && j == '8' && k == '9')) {
+	while (i <= '7') 
+	{
+		int j = i + 1;
+	while (j <= '8')
+	{
+		int k = j + 1;
+	while (k <= '9')
+	{
+		putchar(i);
+		putchar(j);
+		putchar(k);
+		{
+			if (!(i == '7' && j == '8' && k == '9'))
+		       {
                     putchar(',');
                     putchar(' ');
                 }
@@ -29,6 +39,5 @@ int main() {
     }
 
     putchar('\n');
-    return 0;
+	return (0);
 }
-
