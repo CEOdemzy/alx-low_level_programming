b055920708630b54dcb67361f806ca85ba1b159f
CEOdemzy <ud.demzy@gmail.com>
2023-08-11 19:26:24 -0700

    triple numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,42 +1,45 @@
 #include <stdio.h>
 
 /**
- *  main - print three number compilation
+ * main - function to print combination of three digits
  *
- *  Return: o success
+ * Return: always 0 on success
  */
 
 int main(void)
 {
-	int i = '0';
-	int j = '0';
-	int k = '0';
+    int i = '0';
 
-	while (i <= '7')
-	{
-		while (j <= '8')
-		{
-			while (k <= '9')
-			{
-				if (i < j && j < k)
-				{
-					putchar(i);
-					putchar(k);
-					putchar(k);
-					if (!(i == '7' && j == '8' && k == '9'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				k++;
-			}
-			k = '0';
-			j++;
-		}
-		j = '0';
-		i++;
-	}
-	putchar('\n');
-	return (0);	
+    while (i <= '7')
+    {
+        int j = i + 1;
+
+        while (j <= '8')
+        {
+            int k = j + 1;
+
+            while (k <= '9')
+            {
+                putchar(i);
+                putchar(j);
+                putchar(k);
+
+                if (!(i == '7' && j == '8' && k == '9'))
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+
+                k++;
+            }
+
+            j++;
+        }
+
+        i++;
+    }
+
+    putchar('\n');
+    return 0;
 }
+
