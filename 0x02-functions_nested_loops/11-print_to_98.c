2c1b5e83ed146ca3855aa8f763bdda9d615a3288
CEOdemzy <ud.demzy@gmail.com>
2023-08-16 09:38:50 -0700

    98 valuaes

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_to_98 - function that prints all n to 98 natural numbers
+ * @n: the number to start printing from
+ * Return: Always 0.
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(",");
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(",");
+			}
+		}
+	}
+	printf("\n");
+}
