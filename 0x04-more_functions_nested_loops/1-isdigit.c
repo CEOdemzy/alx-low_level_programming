b256b5a751bc5ea003d75efbdf2f7b7a57272aec
CEOdemzy <ud.demzy@gmail.com>
2023-08-17 09:03:47 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isdgit - a function to check for digits 0-9
+ * _isdigit - a function to check for digits 0-9
  * @c: describes integers to be checked
  * Return: 1 if c is a digit and 0 if otherwise
  */
