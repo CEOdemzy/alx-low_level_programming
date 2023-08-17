0436d6db880d9c29538c74549054d9e8de646639
CEOdemzy <ud.demzy@gmail.com>
2023-08-17 15:16:07 -0700

    print straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,16 @@
 #include "main.h"
 /**
- * more_numbers - print 10x number 0-14 then a new line
- *
+ * print_line - print straight line
+ * @n: number of times - is printed
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
