59da4efee603b27589844ca2ca08407f4707b83b
CEOdemzy <ud.demzy@gmail.com>
2023-08-17 15:05:07 -0700

    ten lines and 14 numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,23 +2,17 @@
 
 
 /**
- * print_most_numbers - function to print 0-9 without 2 and 4
- *
- * Return: Always 0
+ * print_most_numbers - prints 0-9 without 2,4 then a new line
+ * Return: void
  */
 void print_most_numbers(void)
 {
-	int c;
-	
-	for (c = 48; c <= 57; c++)
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (c != 50)
-		{
-			if (c != 52)
-			{
-				_putchar(c);
-			}
-		}
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
 	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,4 @@
+#include "main.h"
+/**
+ * more_numbers - print 10x number 0-14 then a new line
+ *
