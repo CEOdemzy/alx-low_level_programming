d59cf888aacb2bbafd5d500417d5c57a376e8d43
CEOdemzy <ud.demzy@gmail.com>
2023-08-17 08:50:21 -0700

    digit 0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdgit - a function to check for digits 0-9
+ * @c: describes integers to be checked
+ * Return: 1 if c is a digit and 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
