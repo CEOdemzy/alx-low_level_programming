2dccfa04ea5781d621bfaaf9a4b6c93ae258ae04
CEOdemzy <ud.demzy@gmail.com>
2023-09-01 19:54:15 -0700

    task 1 arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints number of argument passed into it
+ *
+ * @argc: arggument counts
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc -1);
+	return (0);
+}
