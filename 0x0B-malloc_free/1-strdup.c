68239dd0b3465f3db12c9b4467a125427a1a3863
CEOdemzy <ud.demzy@gmail.com>
2023-09-06 13:39:24 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
-#include "holberton.h"
+#include <stdlib.h>
 
 
 /**
  * _strdup - function to copy string as given parameter
  * @str: the string in view
  * Return: pointer to string on success or null for error
- *?
- char *_strdup(char *str);
- {
- 	char *dupli;
-	unsigned int x, len;
-	
-	x = 0;
-	len = 0;
+ */
+char *_strdup(char *str)
+{
+	char *dupli;
+	int x = 0;
+	int str_len = 0;
 
 	if (str == NULL)
 		return (NULL);
 
-	while (str[len])
-		len++;
-	dupli = malloc(sizeof(char) * (len + 1));
+	while (str[x] != '\0')
+	{
+		str_len++;
+		x++;
+	}
+
+	dupli = malloc(sizeof(char) * (str_len + 1));
 
 	if (dupli == NULL)
 		return (NULL);
 
-	while ((duplicate[x] = str[x]) != '\0')
-		x++;
+	for (x = 0 ; str[x] != '\0' ; x++)
+	{
+		dupli[x] = str[x];
+	}
 
 	return (dupli);
- }
+}
